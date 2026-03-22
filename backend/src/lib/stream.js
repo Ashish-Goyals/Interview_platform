648b755e12e371b7ce932b77efa9758927626c17
Ashish-Goyals <asbarnwal83036@gmail.com>
2026-03-22 21:24:07 +0530

    creating routes and controller logic for video and chat session

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,5 +1,6 @@
 import {StreamChat} from 'stream-chat';
 import {ENV} from './env.js';
+import {StreamClient} from '@stream-io/node-sdk';
 
 const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
@@ -9,8 +10,9 @@ if (!apiKey || !apiSecret) {
     'STREAM_API_KEY or STREAM_API_SECRET is not defined in the environment variables'
   );
 }
+export const streamClient = new StreamClient (apiKey, apiSecret); // Will be used for video calls
 
-export const chatClient = StreamChat.getInstance (apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance (apiKey, apiSecret); //THis is for chat features
 
 export const upsertStreamUser = async userData => {
   try {

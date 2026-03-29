efe4f6da2fea8f1f1ca03aa56fc7bca180247d5f
Ashish-Goyals <asbarnwal83036@gmail.com>
2026-03-29 11:40:10 +0530

    creating home page and make authenticate it

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,12 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const AboutPage = () => {
-  return (
-    <div>
-      about
-
-    </div>
-  );
+  return <div>about</div>;
 };
 
 export default AboutPage;
